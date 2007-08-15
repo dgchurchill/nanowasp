b5264ac696dbd047cd7b9ecf247f354a5ba37956
Dave Churchill <dave@ninthcuriosity.com>
2007-08-15 14:26:23 +0000

    Added icon and version information
    git-svn-id: svn://svn.code.sf.net/p/nanowasp/code/trunk@11 bb216399-ea36-0410-9f3c-cb6ba5a9af6e

---
+++ DiskImageTool/version.h
@@ -0,0 +1,12 @@
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION_STR "1.0"
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+
+#define COPYRIGHT "Copyright © 2007 David G. Churchill"
+#define CONTACT "almostdave@gmail.com"
+
+#endif // VERSION_H
